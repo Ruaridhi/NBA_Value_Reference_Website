ae94ae6d8a179bac02ce8027de942647eb032c67
Sam Symonds <samuelsymonds2017@gmail.com>
2020-06-10 20:01:43 +0100

    Removed http from HomeController

--- NbaPlayerValueApp/Controllers/HomeController.cs
+++ NbaPlayerValueApp/Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Web.Http.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NbaPlayerValueApp.Models;
