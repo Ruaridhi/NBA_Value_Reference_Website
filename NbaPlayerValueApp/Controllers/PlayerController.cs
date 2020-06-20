a973c8473d8e8cf92d0d0b712195537cc35f8a85
Ruaridhi <ruaridhi.oscanlain@lexisnexis.co.uk>
2020-06-20 15:03:18 +0100

    remove year record model add year player record

--- NbaPlayerValueApp/Controllers/PlayerController.cs
+++ NbaPlayerValueApp/Controllers/PlayerController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NbaPlayerValueApp.Models;
 using Newtonsoft.Json;
+// using Newtonsoft.Json.Linq.JObject;
 
 namespace NbaPlayerValueApp.Controllers
 {
@@ -16,14 +17,14 @@ namespace NbaPlayerValueApp.Controllers
     {
 
         [HttpGet]
-        public async Task<List<YearRecord>> Get()
+        public async Task<IndividualPlayerRecord> Get()
         {
             HttpClient httpClient = new HttpClient();
             httpClient.BaseAddress = new Uri("https://nba-tester.firebaseio.com/");
             var responseTask = httpClient.GetAsync(".json");
             responseTask.Wait();
 
-            List<YearRecord> response = await GetFinalResponse(responseTask);
+            IndividualPlayerRecord response = await GetFinalResponse(responseTask);
 
             return response;
 
@@ -31,7 +32,7 @@ namespace NbaPlayerValueApp.Controllers
         }
 
         [HttpGet("{playerId}")]
-        public async Task<List<YearRecord>> Get(string playerId)
+        public async Task<IndividualPlayerRecord> Get(string playerId)
         {
             HttpClient httpClient = new HttpClient();
             httpClient.BaseAddress = new Uri("https://nba-tester.firebaseio.com/");
@@ -39,13 +40,13 @@ namespace NbaPlayerValueApp.Controllers
             var responseTask = httpClient.GetAsync(url);
             responseTask.Wait();
 
-            List<YearRecord> response = await GetFinalResponse(responseTask);
+            IndividualPlayerRecord response = await GetFinalResponse(responseTask);
 
                 return response;
         }
 
         [HttpGet("{teamAbb}/{year}")]
-        public async Task<List<YearRecord>> Get(string teamAbb, int year)
+        public async Task<IndividualPlayerRecord> Get(string teamAbb, int year)
         {
             HttpClient httpClient = new HttpClient();
             httpClient.BaseAddress = new Uri("https://nba-tester.firebaseio.com/");
@@ -53,25 +54,29 @@ namespace NbaPlayerValueApp.Controllers
             var responseTask = httpClient.GetAsync(url);
             responseTask.Wait();
 
-            List<YearRecord> response = await GetFinalResponse(responseTask);
+            IndividualPlayerRecord response = await GetFinalResponse(responseTask);
 
             return response;
         }
 
 
-        private async Task<List<YearRecord>> GetFinalResponse(Task<HttpResponseMessage> responseTask) 
+        private async Task<IndividualPlayerRecord> GetFinalResponse(Task<HttpResponseMessage> responseTask) 
         {
             var result = await responseTask;
-            List<YearRecord> finalResponse = new List<YearRecord>();
+            IndividualPlayerRecord finalResponse = new IndividualPlayerRecord();
             if (result.IsSuccessStatusCode)
             {
-
                 string responseBody = await result.Content.ReadAsStringAsync();
-                Dictionary<string, YearRecord> initialResponse = JsonConvert.DeserializeObject<Dictionary<string, YearRecord>>(responseBody);
-                foreach (KeyValuePair<string, YearRecord> entry in initialResponse)
-                {
-                    finalResponse.Add(entry.Value);
-                }
+                var searchResults = JsonConvert.DeserializeObject<IndividualPlayerRecord>(responseBody);
+                Console.WriteLine(searchResults);
+                // JObject json = JObject.Parse(responseBody);
+                // finalResponse.name = json.name;
+
+                // Dictionary<string, IndividualPlayerRecord> initialResponse = JsonConvert.DeserializeObject<Dictionary<string, IndividualPlayerRecord>>(responseBody);
+                // foreach (KeyValuePair<string, IndividualPlayerRecord> entry in initialResponse)
+                // {
+                //     finalResponse.Add(entry.Value);
+                // }
             }
             else //web api sent error response 
             {
---
+++ NbaPlayerValueApp/Models/IndividualPlayerRecord.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace NbaPlayerValueApp.Models
+{
+    public class IndividualPlayerRecord {
+
+
+        [JsonProperty("name")]
+        public string name { get; set; }
+        [JsonProperty("careerValue")]
+        public int careerValue { get; set; }
+        [JsonProperty("peakMultiplier")]
+        public int peakMultiplier { get; set; }
+
+        
+   
+
+
+        // public int id { get; set; }
+        // public int year { get; set; }
+        // public string league { get; set; }
+        // public string player_id { get; set; }
+        // public string name { get; set; }
+        // public double score { get; set; }
+        // public string position { get; set; }
+        // public int? age { get; set; }
+        // public string team_abbreviation { get; set; }
+        // public double? win_shares { get; set; }
+        // public double? win_shares_48 { get; set; }
+        // public double? vorp { get; set; }
+        // public double? bpm { get; set; }
+        // public int? games { get; set; }
+        // public double? mpg { get; set; }
+        // public string team_full_name { get; set; }
+        // public string team_record { get; set; }
+        // public string team_result { get; set; }
+
+    }
+
+}
